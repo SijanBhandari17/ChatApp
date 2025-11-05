dd36044ededb3e246c183ecdc360fa1f1be53952
Sijan Bhandari <uhisijan@gmail.com>
2025-11-05 14:37:10 +0545

    Backend url fix

--- frontend/src/lib/axiosConfig.js
+++ frontend/src/lib/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getFromLocalStorage, saveToLocalStroage } from './saveToLocalStorage';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'https://chatapp-production-6545.up.railway.app',
 });
 
 console.log('API URL:', import.meta.env.VITE_API_URL);
